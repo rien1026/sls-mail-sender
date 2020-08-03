3e78993d5dd6927e112aa6683d9862b0f1e6f969
SangyeonCho <rien1026@gmail.com>
2020-08-03 13:22:46 +0900

    [del] : unuseful code in Constants.ts

--- src/configs/MailConfig.ts
+++ src/configs/MailConfig.ts
@@ -1,15 +1,14 @@
-
 import * as nodemailer from 'nodemailer';
 import { Constants } from '../utils/Constants';
 
 export const transporter = nodemailer.createTransport({
-    port: Number(Constants.MAIL_CONFIG.MAIL_PORT),
-    host: Constants.MAIL_CONFIG.MAIL_HOST,
-    auth: {
-        user: Constants.MAIL_CONFIG.MAIL_USER,
-        pass: Constants.MAIL_CONFIG.MAIL_PASSWORD,
-    },
-    secure: true,
+	service: 'gmail',
+	port: Number(Constants.MAIL_CONFIG.MAIL_PORT),
+	host: Constants.MAIL_CONFIG.MAIL_HOST,
+	auth: {
+		user: Constants.MAIL_CONFIG.MAIL_USER,
+		pass: Constants.MAIL_CONFIG.MAIL_PASSWORD,
+	},
 });
 
-export const configureMail = () => { }
+export const configureMail = () => {};
--- src/utils/Constants.ts
+++ src/utils/Constants.ts
@@ -1,17 +1,11 @@
-import * as dotenv from 'dotenv';
-
-const PROD_MODE = (process.env['PROD_MODE'] as any) || 'dev';
-dotenv.config({ path: __dirname + '/../../../.env' });
-
 // mail config
 const MAIL_CONFIG = {
-    MAIL_PORT: process.env['MAIL_CONFIG_MAIL_PORT'],
-    MAIL_HOST: process.env['MAIL_CONFIG_MAIL_HOST'],
-    MAIL_USER: process.env['MAIL_CONFIG_MAIL_USER'],
-    MAIL_PASSWORD: process.env['MAIL_CONFIG_MAIL_PASSWORD'],
+	MAIL_PORT: process.env['MAIL_PORT'],
+	MAIL_HOST: process.env['MAIL_HOST'],
+	MAIL_USER: process.env['MAIL_USER'],
+	MAIL_PASSWORD: process.env['MAIL_PASSWORD'],
 };
 
 export const Constants = {
-    PROD_MODE,
-    MAIL_CONFIG,
+	MAIL_CONFIG,
 };
